923a7636ae4713bc642a7514fb085156536c57bc
Hasan KAHRAMAN <kahraman.h@outlook.com>
2022-08-09 14:06:08 +0300

    fix versions

--- components/Detailsline.js
+++ components/Detailsline.js
@@ -8,11 +8,13 @@ function Detailsline({ className, ...props }) {
   return (
     <div  style={{minWidth: "inherit"}} className={styles.Detailsline}>
       <div>{props.Text}</div>
+      <div>
       {props.Value ? (
         <Value className={className} Value={props.Value} />
       ) : (
         'Belirtilmedi'
       )}
+      </div>
     </div>
   );
 }
